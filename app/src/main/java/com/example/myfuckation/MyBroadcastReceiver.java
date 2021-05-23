b9a79b991c571bf4c903374356d4d49f05cc12a8
Eddie321123 <eddieleungproduction@gmail.com>
2021-05-24 02:03:30 +0800

    V0.1

---
+++ app/src/main/java/com/example/myfuckation/MyBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.example.myfuckation;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context aContext, Intent aIntent) {
+        // This is where you start your service
+        aContext.startService(new Intent(aContext, MyService.class));
+    }
+}
---
+++ app/src/main/java/com/example/myfuckation/MyService.java
@@ -0,0 +1,22 @@
+package com.example.myfuckation;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
